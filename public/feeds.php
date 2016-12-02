339a9bedfa79f91006f59123fa4f77eb52f508a5
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-12-02 05:10:48 +0000

    Move public code into it's own directory

--- public/feeds.php
+++ public/feeds.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/lib/bootstrap.php';
+require_once __DIR__ . '/../lib/bootstrap.php';
 
 $feed_addresses = array(
   'http://feeds.feedburner.com/techcrunch/startups.xml',
