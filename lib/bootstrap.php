2f58218fe998d0624539b4a58c467b247b72cb71
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-12-02 05:34:39 +0000

    Remove demo feed class

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -2,6 +2,5 @@
 
 require_once __DIR__ . '/article.php';
 require_once __DIR__ . '/cached_rss_feed.php';
-require_once __DIR__ . '/demo_feed.php';
 require_once __DIR__ . '/feed.php';
 require_once __DIR__ . '/rss_feed.php';
