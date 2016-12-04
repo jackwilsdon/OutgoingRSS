0a4b9913506e30921e6b61e2f9223a6ce55516f9
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-12-04 04:33:57 +0000

    Add comments to build script

--- build-docs.sh
+++ build-docs.sh
@@ -21,21 +21,32 @@ fail() {
 }
 
 cp "${JSDOC_CONFIGURATION}" "${JSDOC_TEMPORARY_CONFIGURATION}" || fail
+
+# Save the current state of the branch and switch branches
 git stash save -u -q || fail
 git checkout -q "${SOURCE_BRANCH}" || fail
+
+# Build the documentation
 jsdoc -c "${JSDOC_TEMPORARY_CONFIGURATION}" -d "${JSDOC_TEMPORARY_OUTPUT}" || fail
+
+# Switch back branches
 git checkout -q "${ORIGINAL_BRANCH}" || fail
+
+# Delete the existing JSDoc output and copy the generated documentation over it
 rm -rf "${JSDOC_OUTPUT}" || fail
 mv "${JSDOC_TEMPORARY_OUTPUT}" "${JSDOC_OUTPUT}" || fail
 
+# Check if any files have changed
 if [ -n "$(git status -s)" ]; then
+    # Add the generated documentation if it has changed
     git add "${JSDOC_OUTPUT}" || fail
-
     git commit -m "${1:-Update documentation}" || fail
 else
     echo "No changes to documentation"
 fi
 
+# Restore the previous state of the branch
 git stash apply -q > /dev/null || fail
 
+# Delete the generated temporary configuration file
 rm "${JSDOC_TEMPORARY_CONFIGURATION}"
